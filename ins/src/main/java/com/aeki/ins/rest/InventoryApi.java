ed2f193a76aa31160d2f07725205512b25ee9294
ammadnaseer <ammad.naseer@yahoo.com>
2022-06-01 23:38:26 +0200

    Removed unused java files. Added swagger documentation. Enabled flyway

--- ins/src/main/java/com/aeki/ins/rest/InventoryApi.java
+++ ins/src/main/java/com/aeki/ins/rest/InventoryApi.java
@@ -2,6 +2,12 @@ package com.aeki.ins.rest;
 
 import com.aeki.ins.exception.ProductNotfoundException;
 import com.aeki.ins.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.awt.print.Book;
 import java.util.List;
 
 @Log4j2
@@ -22,19 +29,34 @@ public class InventoryApi {
         this.productService = productService;
     }
 
+
+    @Operation(summary = "Gets the list of product with name and remaining quantity")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retrieves a list of available products",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = AvailableProducts.class))})
+    })
     @GetMapping("products")
     public ResponseEntity<List<AvailableProducts>> getAllProductsWithQuantity() {
         return ResponseEntity.ok(productService.getAllProductsWithQuantity());
     }
 
+    @Operation(summary = "Deletes a product by its name, can be used for sell functionality")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "The product was removed",
+                    content = {@Content(mediaType = "text/plain;charset=UTF-8",
+                            schema = @Schema(implementation = String.class))}),
+            @ApiResponse(responseCode = "404", description = "Product that you are trying to delete could not be found",
+                    content = @Content)})
     @DeleteMapping("/product/{name}")
-    public ResponseEntity<String> sellProduct(@PathVariable String name) {
+    public ResponseEntity<String> sellProduct(@Parameter(description = "name of the product")
+                                              @PathVariable String name) {
         try {
             productService.removeProductByName(name);
             return ResponseEntity.ok("product named as " + name + " removed!");
-        }catch(ProductNotfoundException productNotfoundException){
+        } catch (ProductNotfoundException productNotfoundException) {
             log.error("error", productNotfoundException.getMessage());
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product named "+name+" doesnot exist", productNotfoundException);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product named " + name + " doesnot exist", productNotfoundException);
         }
 
     }
